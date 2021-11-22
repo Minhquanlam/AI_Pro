9e59bcd4cfbaf52ba91b1f91a920fe1f1af357d8
Minhquanlam <93149325+Minhquanlam@users.noreply.github.com>
2021-11-22 19:16:14 +0700

    Sửa thành đọc file pkl và show kết quả

--- Finalexample/submit_form/finaltest.py
+++ Finalexample/submit_form/finaltest.py
@@ -1,52 +1,21 @@
 # -*- coding: utf-8 -*-
-import pandas as pd
-import sys
-data = pd.read_csv('C:/Users/lmqua/Finalexample/submit_form/index.php', encoding="latin-1") 
-# Ông copy path riêng đường link máy ông để đọc data vào spam.csv
+Import pickle
 
-import numpy as np
-from pandas import DataFrame
-from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.naive_bayes import BernoulliNB
-from sklearn.metrics import accuracy_score, confusion_matrix
-from sklearn.feature_extraction.text import CountVectorizer
-
-le = preprocessing.LabelEncoder()
-data['Label'] = le.fit_transform(data['Label'])
-
-x=data['EmailText']
-y=data['Label']
-
-cv = CountVectorizer()
-x = cv.fit_transform(x).toarray()
-
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
-
-from sklearn.naive_bayes import GaussianNB
-model1 = GaussianNB()
-model1.fit(x_train, y_train)
-y_predicted1 = model1.predict(x_test)
-accuracy_score(y_test, y_predicted1)
-print("Gaussian Naive Bayes accuracy score:\t", accuracy_score(y_test, y_predicted1) * 100, "%")
-
-#Chay ouput
-input=sys.argv[1]
-
-input = cv.fit_transform(input).toarray()
-prediction = model1.predict(input)
-
+model=pickle.load(open("spam.pkl","rb")
+cv=pickle.load(open("vectorizer.pkl","rb")
+               
+               
+msg=" "
+data=[msg]
+vect=cv.transform(data).toarray()
+prediction=model.predict(vect)
 result=prediction[1]
 if result==1:
-    print('<h3>This is a spam mail</h3>')
-
+      print('<h3>This is a spam mail</h3>')
 else:
-    print('<h3>This is a ham mail</h3>')
-
-
+      print('<h3>This is a ham mail</h3>')
+               
+         
 
 
 
